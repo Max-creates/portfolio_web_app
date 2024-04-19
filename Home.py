249c5f351fe44481fc190c66367ee1ff1e2d2525
Max <maksym.solomyanov@gmail.com>
2024-04-19 20:42:14 +0200

    Add new link.

--- Home.py
+++ Home.py
@@ -26,36 +26,19 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[11:].iterrows():
+    for index, row in df[14:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
         
 with col4:
-    for index, row in df[:11].iterrows():
+    for index, row in df[:14].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
         
-content3 = """
 
-Below you can find all of my certificates and links.
-
-"""
-
-st.header(content3)
-
-col5, empty_col = st.columns(2)
-
-df_certificates = pd.read_csv("certificates.csv", sep=";")
-
-with col5:
-    for index, row in df_certificates.iterrows():
-
-        st.image("certificates/" + row["image"])
-        st.write(row["description"])
-        st.write(f"[Link]({row['url']})")
 
         
